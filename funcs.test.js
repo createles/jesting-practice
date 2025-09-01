42d30dc913c9772f27202fb694a6b1fa31af4b08
createlsworklappy <justinecastillo7@gmail.com>
2025-09-01 12:15:19 +0900

    add tests for caesarCipher and analyzeArray

--- funcs.test.js
+++ funcs.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from "./funcs";
+import { capitalize, reverseString, calculator, caesarCipher, analyzeArray } from "./funcs";
 
 describe("string functions test", () => {
   test("first letter is capitalized", () => {
@@ -29,5 +29,36 @@ describe("calculator functions test", () => {
 
   test("multiplication function check", () => {
     expect(calculator.multiply(4, 2)).toBe(8);
+  });
+});
+
+describe("caesar cipher test", () => {
+  test("lower case test", () => {
+    expect(caesarCipher("test", 5)).toBe("yjxy");
+  });
+
+  test("upper case test", () => {
+    expect(caesarCipher("TEST", 5)).toBe("YJXY");
+  });
+
+  test("mixed case test", () => {
+    expect(caesarCipher("TeSt", 5)).toBe("YjXy");
+  });
+
+  test("with punctuation test", () => {
+    expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+  });
+})
+
+describe("analyze array test", () => {
+  test("returns proper object", () => {
+    const checkObject = {
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6,
+    };
+
+    expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual(checkObject);
   });
 });
